cd14c619181523f82f1addc6af847a0978262980
dv.kulikov <dv.kulikov@mail.ru>
2020-10-28 21:43:25 +0300

    Initial Commit

---
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 26.10.2020.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты на экране
+    // 4. Избавится от кнопки возврата на предыдущий экран
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+    }
+
+}
